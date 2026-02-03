06bef59a0989172532b34b5f9df848d40b0fef0a
Luis Sotomayor <lsbvks77@gmail.com>
2026-02-03 12:30:01 +0100

    create/ connectDB

---
+++ backend/src/config/db.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.DB_URL);
+        console.log("Conectado con éxito");
+    } catch (error) {
+        console.log("Fallo en la conexión con la base de datos");    }
+};
+
+module.exports = { connectDB };
