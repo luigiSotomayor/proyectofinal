b07383a7ebca2db89f5b4b238ecf70c71524871e
Luis Sotomayor <lsbvks77@gmail.com>
2026-02-05 08:36:59 +0100

    create/ middleware hasRole.js

---
+++ backend/src/middlewares/hasRole.js
@@ -0,0 +1,15 @@
+const hasRole = (roles = []) => {
+  return (req, res, next) => {
+    if (!req.user) {
+      return res.status(401).json("Not authenticated");
+    }
+
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json("Access denied");
+    }
+
+    next();
+  };
+};
+
+export default hasRole;
