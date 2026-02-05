f4830f1a4d9b782b009c74b2a4afef9a070d4890
Luis Sotomayor <lsbvks77@gmail.com>
2026-02-05 08:47:31 +0100

    update/ routes/user.js con middleware hasRole.js

--- backend/src/api/routes/user.js
+++ backend/src/api/routes/user.js
@@ -1,10 +1,11 @@
 import { getUsers, getUser, postUser, updateUser, deleteUser, login } from '../controllers/user.js';
 import isAuth from '../../middlewares/isAuth.js';
+import hasRole from '../../middlewares/hasRole.js';
 
 import express from 'express';
 const userRouter = express.Router();
 
-userRouter.get("/", isAuth, getUsers);
+userRouter.get("/", isAuth, hasRole["director deportivo"], getUsers);
 userRouter.get("/:id", getUser);
 userRouter.post("/", postUser); 
 userRouter.post("/login", login);
