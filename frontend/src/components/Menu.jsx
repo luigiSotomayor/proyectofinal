7e3cd829c8934917c353637f99260ee99ab0215b
Luis Sotomayor <lsbvks77@gmail.com>
2026-02-24 23:12:51 +0100

    create/ button de logout

--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import Button from "./Button.jsx";
+import { useAuth } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 const Menu = () => {
-  return (
-    <div>Menu</div>
-  )
-}
+  const { logout } = useAuth();
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    logout();
+    navigate("/");
+  };
 
-export default Menu
+  return (
+    <div className="menu">
+      <section className="logout">
+        <Button
+          type="button"
+          text="Logout"
+          onClick={handleSubmit}
+        />
+      </section>
+      <section className="usuarios">
+        <h2>Usuarios</h2>
+      </section>
+      <section className="equipos">
+        <h2>Equipos</h2>
+      </section>
+      <section className="partidos">
+        <h2>Partidos</h2>
+      </section>
+    </div>
+  );
+};
+
+export default Menu;
--- frontend/src/components/UserSubHeader.jsx
+++ frontend/src/components/UserSubHeader.jsx
@@ -1,14 +1,11 @@
-import React from "react";
-import { useState, useEffect } from "react";
 import { useAuth } from "../context/AuthContext.jsx";
 
 const UserSubHeader = () => {
-  const [data, setData] = useState(null);
   const { user } = useAuth();
 
   return (
     <div className="usersubheader">
-      {user.role}: {user.firstName} {user.lastName} 
+      {user.role}: {user.firstName} {user.lastName}
     </div>
   );
 };
