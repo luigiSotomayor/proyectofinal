e24a87a6e3fcafc6c0e95f6090b291ca726f8373
Luis Sotomayor <lsbvks77@gmail.com>
2026-02-25 13:56:43 +0100

    fix/ consulta equipos por id jugador

--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -1,11 +1,31 @@
-import React from "react";
 import Button from "./Button.jsx";
 import { useAuth } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { apiFetch } from "../utils/apiFetch.js";
 
 const Menu = () => {
   const { logout, user } = useAuth();
   const navigate = useNavigate();
+  const [teamOfPlayer, setTeamOfPlayer] = useState({});
+  const [matches, setMatches] = useState([]);
+  const [selectedMatch, setSelectedMatch] = useState(null);
+
+  useEffect(() => {
+    const loadMatches = async () => {
+      try {
+        const teamReq = await apiFetch(`http://localhost:3000/api/v1/team/team/${user._id}`);
+        setTeamOfPlayer(teamReq);
+        const matchesReq = await apiFetch(`http://localhost:3000/api/v1/match/team/${teamReq._id}`);
+        setMatches(matchesReq);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    loadMatches();
+  }, []);
+
   const handleSubmit = (e) => {
     e.preventDefault();
     logout();
@@ -27,10 +47,11 @@ const Menu = () => {
           <h2>Equipos</h2>
         </section>
       )}
-
-      <section className="partidos">
-        <h2>Partidos</h2>
-      </section>
+      {user.role === "jugador" && (
+        <section className="partidos">
+          <h2>{matches.length} partidos</h2>
+        </section>
+      )}
     </div>
   );
 };
--- frontend/src/utils/apiFetch.js
+++ frontend/src/utils/apiFetch.js
@@ -1,12 +1,19 @@
 export const apiFetch = async (url, options = {}) => {
   const token = localStorage.getItem("token");
 
-  return fetch(url, {
+  const response = await fetch(url, {
     ...options,
     headers: {
-      "Content-Type": "application/json",
       Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
       ...options.headers,
     },
   });
+  const resp = await response.json();
+
+  if (!response.ok) {
+    throw new Error("Error en la petición");
+  }
+
+  return resp;
 };
